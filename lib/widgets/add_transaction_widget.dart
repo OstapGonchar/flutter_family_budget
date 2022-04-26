a467ffe026919c69d25474688176e48599a26612
OstapGonchar <ostap.gonchar@gmail.com>
2022-04-26 16:16:34 +0200

    Added more fields

--- lib/widgets/add_transaction_widget.dart
+++ lib/widgets/add_transaction_widget.dart
@@ -29,94 +29,12 @@ class _AddTransactionState extends State<AddTransactionWidget> {
       body: Center(
         child: ListView(
           children: [
-            Row(
-              children: [
-                label('Account:'),
-                Expanded(
-                    child: DropdownButton(
-                        value: _accountValue,
-                        isExpanded: true,
-                        items: <String>['Nordea', 'Wallet']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _accountValue = newValue!;
-                          });
-                        }))
-              ],
-            ),
-            Row(
-              children: [
-                label('Budget:'),
-                Expanded(
-                    child: DropdownButton(
-                        value: _budgetName,
-                        isExpanded: true,
-                        items: <String>['April 2022']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _budgetName = newValue!;
-                          });
-                        }))
-              ],
-            ),
-            Row(
-              children: [
-                label('Date:'),
-                Expanded(
-                    child: TextField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                  ),
-                  controller: _transactionDateTextController,
-                )),
-                IconButton(
-                  icon: const Icon(Icons.calendar_today),
-                  onPressed: _selectDate,
-                )
-              ],
-            ),
-            Row(
-              children: [
-                label('Sum:'),
-                Expanded(
-                    child: TextField(
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(
-                        RegExp(r'^(\d+)?\.?\d{0,2}'))
-                  ],
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                  ),
-                )),
-                DropdownButton(
-                    value: _defaultCurrency,
-                    items: <String>['DKK', 'USD']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _defaultCurrency = newValue!;
-                      });
-                    })
-              ],
-            )
+            accountRow(),
+            budgetRow(),
+            dateRow(),
+            sumRow(),
+            titleRow(),
+            descriptionRow()
           ],
         ),
       ),
@@ -143,6 +61,138 @@ class _AddTransactionState extends State<AddTransactionWidget> {
         ],
       ),
     );
+  }
+
+  Row accountRow() {
+    return Row(
+            children: [
+              label('Account:'),
+              Expanded(
+                  child: DropdownButton(
+                      value: _accountValue,
+                      isExpanded: true,
+                      items: <String>['Nordea', 'Wallet']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _accountValue = newValue!;
+                        });
+                      }))
+            ],
+          );
+  }
+
+  Row budgetRow() {
+    return Row(
+            children: [
+              label('Budget:'),
+              Expanded(
+                  child: DropdownButton(
+                      value: _budgetName,
+                      isExpanded: true,
+                      items: <String>['April 2022']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _budgetName = newValue!;
+                        });
+                      }))
+            ],
+          );
+  }
+
+  Row dateRow() {
+    return Row(
+            children: [
+              label('Date:'),
+              Expanded(
+                  child: TextField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter transaction date'
+                ),
+                controller: _transactionDateTextController,
+              )),
+              IconButton(
+                icon: const Icon(Icons.calendar_today),
+                onPressed: _selectDate,
+              )
+            ],
+          );
+  }
+
+  Row sumRow() {
+    return Row(
+            children: [
+              label('Sum:'),
+              Expanded(
+                  child: TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^(\d+)?\.?\d{0,2}'))
+                ],
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter transaction sum'
+                ),
+              )),
+              DropdownButton(
+                  value: _defaultCurrency,
+                  items: <String>['DKK', 'USD']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _defaultCurrency = newValue!;
+                    });
+                  })
+            ],
+          );
+  }
+
+  Row titleRow() {
+    return Row(
+            children: [
+              label('Title:'),
+              const Expanded(
+                  child: TextField(
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter transaction title'
+                ),
+              )),
+            ],
+          );
+  }
+
+  Row descriptionRow() {
+    return Row(
+            children: [
+              label('Description:'),
+              const Expanded(
+                  child: TextField(
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter transaction description'
+                ),
+              )),
+            ],
+          );
   }
 
   void _selectDate() async {
