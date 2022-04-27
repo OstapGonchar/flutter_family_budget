0b1e1eb8978e697ae0d9de12b7a84addebf97ac7
OstapGonchar <ostap.gonchar@gmail.com>
2022-04-27 15:34:43 +0200

    Added Form validators

--- lib/widgets/add_transaction_widget.dart
+++ lib/widgets/add_transaction_widget.dart
@@ -1,3 +1,4 @@
+import 'package:family_budget/utils/validators/form_validators.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
@@ -14,6 +15,7 @@ class AddTransactionWidget extends StatefulWidget {
 class _AddTransactionState extends State<AddTransactionWidget> {
   final _transactionDateTextController = TextEditingController();
   final DateFormat dateFormat = DateFormat("yyyy-MM-dd");
+  final _formKey = GlobalKey<FormState>();
 
   String _accountValue = 'Nordea';
   String _budgetName = 'April 2022';
@@ -26,7 +28,8 @@ class _AddTransactionState extends State<AddTransactionWidget> {
       appBar: AppBar(
         title: const Text('Add transaction'),
       ),
-      body: Center(
+      body: Form(
+        key: _formKey,
         child: ListView(
           children: [
             accountRow(),
@@ -42,11 +45,14 @@ class _AddTransactionState extends State<AddTransactionWidget> {
         children: [
           Expanded(
               child: Container(
-                padding: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
                   height: 60,
                   child: ElevatedButton(
                       onPressed: () {
-                        //TODO: Add add logic
+                        if (_formKey.currentState!.validate()) {
+                          //TODO: Add add logic
+                          debugPrint('Valid, add logic here');
+                        }
                       },
                       child: const Text('Add')))),
           Expanded(
@@ -63,136 +69,146 @@ class _AddTransactionState extends State<AddTransactionWidget> {
     );
   }
 
+  @override
+  void dispose() {
+    _transactionDateTextController.dispose();
+    super.dispose();
+  }
+
   Row accountRow() {
     return Row(
-            children: [
-              label('Account:'),
-              Expanded(
-                  child: DropdownButton(
-                      value: _accountValue,
-                      isExpanded: true,
-                      items: <String>['Nordea', 'Wallet']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _accountValue = newValue!;
-                        });
-                      }))
-            ],
-          );
+      children: [
+        label('Account:'),
+        Expanded(
+            child: DropdownButtonFormField(
+          value: _accountValue,
+          isExpanded: true,
+          items: <String>['Nordea', 'Wallet']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (String? newValue) {
+            setState(() {
+              _accountValue = newValue!;
+            });
+          },
+          validator: isEmptyStringValidator(),
+        ))
+      ],
+    );
   }
 
   Row budgetRow() {
     return Row(
-            children: [
-              label('Budget:'),
-              Expanded(
-                  child: DropdownButton(
-                      value: _budgetName,
-                      isExpanded: true,
-                      items: <String>['April 2022']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _budgetName = newValue!;
-                        });
-                      }))
-            ],
-          );
+      children: [
+        label('Budget:'),
+        Expanded(
+            child: DropdownButtonFormField(
+          value: _budgetName,
+          isExpanded: true,
+          items: <String>['April 2022']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (String? newValue) {
+            setState(() {
+              _budgetName = newValue!;
+            });
+          },
+          validator: isEmptyStringValidator(),
+        ))
+      ],
+    );
   }
 
   Row dateRow() {
     return Row(
-            children: [
-              label('Date:'),
-              Expanded(
-                  child: TextField(
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter transaction date'
-                ),
-                controller: _transactionDateTextController,
-              )),
-              IconButton(
-                icon: const Icon(Icons.calendar_today),
-                onPressed: _selectDate,
-              )
-            ],
-          );
+      children: [
+        label('Date:'),
+        Expanded(
+            child: TextFormField(
+          readOnly: true,
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter transaction date'),
+          controller: _transactionDateTextController,
+          validator: isEmptyStringValidator(),
+        )),
+        IconButton(
+          icon: const Icon(Icons.calendar_today),
+          onPressed: _selectDate,
+        )
+      ],
+    );
   }
 
   Row sumRow() {
     return Row(
-            children: [
-              label('Sum:'),
-              Expanded(
-                  child: TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.allow(
-                      RegExp(r'^(\d+)?\.?\d{0,2}'))
-                ],
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter transaction sum'
-                ),
-              )),
-              DropdownButton(
-                  value: _defaultCurrency,
-                  items: <String>['DKK', 'USD']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _defaultCurrency = newValue!;
-                    });
-                  })
-            ],
-          );
+      children: [
+        label('Sum:'),
+        Expanded(
+            child: TextFormField(
+          keyboardType: TextInputType.number,
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.allow(RegExp(r'^(\d+)?\.?\d{0,2}'))
+          ],
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter transaction sum'),
+          validator: isEmptyStringValidator(),
+        )),
+        DropdownButton(
+            value: _defaultCurrency,
+            items: <String>['DKK', 'USD']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                _defaultCurrency = newValue!;
+              });
+            })
+      ],
+    );
   }
 
   Row titleRow() {
     return Row(
-            children: [
-              label('Title:'),
-              const Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter transaction title'
-                ),
-              )),
-            ],
-          );
+      children: [
+        label('Title:'),
+        Expanded(
+            child: TextFormField(
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter transaction title'),
+          validator: isEmptyStringValidator(),
+        )),
+      ],
+    );
   }
 
   Row descriptionRow() {
     return Row(
-            children: [
-              label('Description:'),
-              const Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter transaction description'
-                ),
-              )),
-            ],
-          );
+      children: [
+        label('Description:'),
+        Expanded(
+            child: TextFormField(
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter transaction description'),
+          validator: isEmptyStringValidator(),
+        )),
+      ],
+    );
   }
 
   void _selectDate() async {
