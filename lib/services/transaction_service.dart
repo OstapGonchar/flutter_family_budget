1095175c8446dec0073ed5f032f3cb0c83691270
OstapGonchar <ostap.gonchar@gmail.com>
2022-04-28 10:05:39 +0200

    Aligned with model update

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -3,9 +3,9 @@ import 'package:family_budget/models/transaction.dart';
 class TransactionService {
   List<Transaction> getTransactions() {
     return [
-      Transaction(1, 100, "USD", DateTime(2022, 4, 12)),
-      Transaction(2, 10, "USD", DateTime(2022, 4, 13)),
-      Transaction(3, 20, "USD", DateTime(2022, 4, 13)),
+      Transaction(100, "USD", DateTime(2022, 4, 12)),
+      Transaction(10, "USD", DateTime(2022, 4, 13)),
+      Transaction(20, "USD", DateTime(2022, 4, 13)),
     ];
   }
 }
