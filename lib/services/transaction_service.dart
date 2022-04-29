23b10b6cf1bcd6b43eaf204693cbeab9bb475d83
OstapGonchar <ostap.gonchar@gmail.com>
2022-04-29 14:27:56 +0200

    Add files via upload

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,11 +1,13 @@
+import 'package:family_budget/models/account.dart';
 import 'package:family_budget/models/transaction.dart';
 
 class TransactionService {
   List<Transaction> getTransactions() {
+    var account = const Account('Nordea', id: 1);
     return [
-      Transaction(100, "USD", DateTime(2022, 4, 12)),
-      Transaction(10, "USD", DateTime(2022, 4, 13)),
-      Transaction(20, "USD", DateTime(2022, 4, 13)),
+      Transaction('Transaction 1', 100, "USD", DateTime(2022, 4, 12), account),
+      Transaction('Transaction 2', 10, "USD", DateTime(2022, 4, 13), account),
+      Transaction('Transaction 3', 20, "USD", DateTime(2022, 4, 13), account),
     ];
   }
 }
